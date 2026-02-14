d50c9c1eb3088782f5b5fd59b624bc396fc32bd4
João Couto <setemais07@gmail.com>
2026-02-13 22:44:24 -0300

    Deploy compiled Remotion bundle

---
+++ build/845.bundle.js
@@ -0,0 +1,11 @@
+(self["webpackChunkshorts_factory_ai"] = self["webpackChunkshorts_factory_ai"] || []).push([[845],{
+
+/***/ 1845
+() {
+
+/* (ignored) */
+
+/***/ }
+
+}]);
+//# sourceMappingURL=845.bundle.js.map
